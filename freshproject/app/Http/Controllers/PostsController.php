6483f75871d4c4746d679daf967e8d2cc39b21b7
Nathan Merry <nathan.merry@investoogroup.com>
2020-02-14 10:37:44 +0000

    Hello Eloquent

--- freshproject/app/Http/Controllers/PostsController.php
+++ freshproject/app/Http/Controllers/PostsController.php
@@ -4,21 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => "This is my first blog post",
-            'my-second-post' => "This is my second post"
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, "sorry that page wasn't found");
-        }
-
         return view('post', [
-            'post' => $posts[$post]
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
